28b843a98e9c62ffbae6b6216f28c9aa30297e08
alpylmz <ylmz.alp.e@gmail.com>
2022-02-22 15:16:03 +0300

    ros topics all information

--- scripts/ros1_info.py
+++ scripts/ros1_info.py
@@ -14,8 +14,66 @@ class ROS1:
         output = output.split()
         i = 0
         while i < len(output):
-            # convert from bytes to string
-            output[i] = output[i].decode("utf-8")[1:]
+            topic_name = output[i].decode("utf-8")
+
+            p2 = Popen(["rostopic", "info", topic_name], stdout=PIPE, stderr=PIPE)
+            output2, error2 = p2.communicate()
+            if p2.returncode != 0:
+                return "ERROR! " + error2.decode() + " with error code " + str(p2.returncode)
+            
+            output2 = output2.split()
+            print("---------------------------")
+            print("Splitted topic is: " + str(output2))
+            print("---------------------------")
+
+            # for now
+            # 0 is "Type"
+            # 1 is the type
+            # 2 is publishers
+            # 3 is *
+            # 4 is first publisher
+            # 5 is the URI of the publisher
+            # 6 is * if there are more publishers
+            # otherwise, it is "Subscribers:" 
+            # the same loop again...
+
+            type_of_topic = output2[1].decode("utf-8")
+
+            publisher_cnt = 3
+            publishers = []
+            while True:
+                # that means publishers are over
+                if output2[publisher_cnt].decode("utf-8") != "*":
+                    if output2[publisher_cnt].decode("utf-8") == "None":
+                        publisher_cnt += 1
+                    print("Different!", output2[publisher_cnt])
+                    break
+                
+                publisher_name = output2[publisher_cnt+1].decode("utf-8")
+                publisher_uri = output2[publisher_cnt+2].decode("utf-8")
+                publishers.append({"name": publisher_name, "uri": publisher_uri})
+
+                publisher_cnt += 3
+
+            subscribers_cnt = publisher_cnt + 1
+            subscribers = []
+            while True:
+                if subscribers_cnt >= len(output2):
+                    break
+                # that means subscribers are over
+                if output2[subscribers_cnt].decode("utf-8") != "*":
+                    print("Different!", output2[subscribers_cnt])
+
+                    break
+                
+                subscriber_name = output2[subscribers_cnt+1].decode("utf-8")
+                subscriber_uri = output2[subscribers_cnt+2].decode("utf-8")
+                subscribers.append({"name": subscriber_name, "uri": subscriber_uri})
+
+                subscribers_cnt += 3
+
+
+            output[i] = {"name": topic_name, "type": type_of_topic, "publishers": publishers, "subscribers": subscribers}
             i += 1
         return output
 
@@ -30,7 +88,6 @@ class ROS1:
         output = output.split()
         i = 0
         while i < len(output):
-            # convert from bytes to string
             output_together = output[i].decode("utf-8")
             # separate namespace
             output[i] = {"namespace": output_together[:output_together.find('/')+1], "node_name": output_together[output_together.find('/')+1:]}
